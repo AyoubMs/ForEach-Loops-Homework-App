c19f984c6c7f6d922a5a464f60c05625a8df9f95
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:58:58 +0200

    add app

---
+++ ForEachLoopsHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string? firstName;
+bool isInvalid;
+int dumb;
+
+List<string> firstNames = new List<string>();
+
+do
+{
+    Console.Write("Add a first name (or exit): ");
+
+    firstName = Console.ReadLine();
+
+    isInvalid = int.TryParse(firstName, out dumb);
+
+    if (isInvalid || string.IsNullOrEmpty(firstName)) Console.WriteLine("Please enter a valid name");
+
+    if (!isInvalid && firstName != "exit" && !string.IsNullOrEmpty(firstName)) firstNames.Add(firstName);
+
+    if (firstName == "exit")
+    {
+        foreach (string name in firstNames)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+
+} while (firstName != "exit");
+
